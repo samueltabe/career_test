7eb34ec4ee5f8e6d904cabae5d2b06f0cd7fdea8
Samuel Tabe <61163104+samueltabe@users.noreply.github.com>
2024-06-10 20:02:57 +0100

    initial commit

---
+++ resources/views/layouts/inc/mobilesidebar.blade.php
@@ -0,0 +1,45 @@
+<div class="scrollable">
+    <a href="javascript:;" class="mobile-menu-toggler"> <i data-lucide="x-circle" class="w-8 h-8 text-white transform -rotate-90"></i> </a>
+    <ul class="scrollable__content py-2">
+        <li>
+            <a href="javascript:;.html" class="menu menu--active">
+                <div class="menu__icon"> <i data-lucide="home"></i> </div>
+                <div class="menu__title"> Dashboard <i data-lucide="chevron-down" class="menu__sub-icon transform rotate-180"></i> </div>
+            </a>
+            <ul class="menu__sub-open">
+                <li>
+                    <a href="side-menu-light-dashboard-overview-1.html" class="menu menu--active">
+                        <div class="menu__icon"> <i data-lucide="activity"></i> </div>
+                        <div class="menu__title"> Overview 1 </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="side-menu-light-dashboard-overview-2.html" class="menu">
+                        <div class="menu__icon"> <i data-lucide="activity"></i> </div>
+                        <div class="menu__title"> Overview 2 </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="side-menu-light-dashboard-overview-3.html" class="menu">
+                        <div class="menu__icon"> <i data-lucide="activity"></i> </div>
+                        <div class="menu__title"> Overview 3 </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="index.html" class="menu">
+                        <div class="menu__icon"> <i data-lucide="activity"></i> </div>
+                        <div class="menu__title"> Overview 4 </div>
+                    </a>
+                </li>
+            </ul>
+        </li>
+        <li>
+        <li>
+            <a href="side-menu-light-inbox.html" class="menu">
+                <div class="menu__icon"> <i data-lucide="inbox"></i> </div>
+                <div class="menu__title"> Inbox </div>
+            </a>
+        </li>
+        <li class="menu__devider my-6"></li>
+    </ul>
+</div>
